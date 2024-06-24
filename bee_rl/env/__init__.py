48a161a95f9d61dfc8db4aff5a23046916739e8e
Anton Kovalov <anton.kov.hack@proton.me>
2024-06-24 21:36:18 +0200

    feat: port control aviary
    * Allows to target both desired position and desired speed at that
        point. Seems to be the most promising for work with curves.

--- bee_rl/env/__init__.py
+++ bee_rl/env/__init__.py
@@ -1,5 +1,6 @@
 from .base_aviary import BaseAviary
 from .base_rl_aviary import BaseRLAviary
 from .vel_aviary import VelocityAviary
+from .ctrl_aviary import CtrlAviary
 
-__all__ = ["BaseAviary", "BaseRLAviary", "VelocityAviary"]
+__all__ = ["BaseAviary", "BaseRLAviary", "VelocityAviary", "CtrlAviary"]
