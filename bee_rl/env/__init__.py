920800116e998d671aeaafae8b88d83b0cddcf98
Anton Kovalov <anton.kov.hack@proton.me>
2024-06-29 14:28:53 +0200

    chore: remove velocity based env

--- bee_rl/env/__init__.py
+++ bee_rl/env/__init__.py
@@ -1,6 +1,5 @@
 from .base_aviary import BaseAviary
 from .base_rl_aviary import BaseRLAviary
-from .vel_aviary import VelocityAviary
 from .ctrl_aviary import CtrlAviary
 
-__all__ = ["BaseAviary", "BaseRLAviary", "VelocityAviary", "CtrlAviary"]
+__all__ = ["BaseAviary", "BaseRLAviary", "CtrlAviary"]
